88c3514ffc9721e32cb874fd0f050b44ccf48e7c
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-06-01 15:56:37 +1000

    Finished Project
    -Completed delete application of notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,22 @@ import CreateArea from "./CreateArea";
 function App() {
   // const [currentTitle, setCT] = useState("");
   // const [currentContent, setContent] = useState("");
-  const [noteArray, setNoteArray] = useState("");
+  const [notes, setNotes] = useState([]);
 
   function addNote(note) {
-    console.log(note);
+    // console.log(note);
+    console.log(notes);
+    setNotes(prevNotes => {
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNotes(id) {
+    setNotes(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
   }
 
   return (
@@ -23,7 +35,21 @@ function App() {
         // content={currentContent}
         // setContent={setContent}
       />
-      <Note key={1} title="Note title" content={"Note content"} />
+      {notes.map((noteItem, index) => {
+        // console.log(noteItem);
+        console.log(index);
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            del={deleteNotes}
+          />
+        );
+      })}
+      {/* <Note key={1} title="Note title" content={"Note content"} /> */}
+
       <Footer />
     </div>
   );
